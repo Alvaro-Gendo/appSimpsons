d39cf7e5071be48852ac72e6f5523d00ddfbc25c
Alvaro Ruiz Mejia <zhuone@hotmail.com>
2022-09-25 17:29:47 -0300

    agregue hoja de css y react-bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Container } from "react-bootstrap";
+import "./style.css"
+
+function App() {
+  return (
+  <>
+  <Container className="">
+    <h1>Prueba de react-bootstrap</h1>
+  </Container>
+  </>
+  );
+}
+
+export default App;
