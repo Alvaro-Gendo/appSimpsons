dd8b31dc79100af8bee1406c6a4503a0df9e453c
Alvaro Ruiz Mejia <zhuone@hotmail.com>
2022-10-04 17:36:42 -0300

    agregue el spinner como componente condicional

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect, useState } from "react";
 import { Button, Container } from "react-bootstrap";
 import Frase from "./components/Frase";
+import Spinner from "./components/Spinner";
 import "./style.css";
 
 
 function App() {
   const [personaje, setPersonaje] = useState({});
+  const [mostrarSpinner, setMostrarSpinner] = useState(true)
 
   useEffect(()=>{
     consultarAPI();
@@ -14,20 +16,25 @@ function App() {
   },[])
 
   const consultarAPI = async() =>{
-    
-    
+      //mostrar el spinner
+    setMostrarSpinner(true)
     try {
       const respuesta = await fetch("https://thesimpsonsquoteapi.glitch.me/quotes");
       // console.log(respuesta)
       const dato = await respuesta.json();
       // console.log(dato[0])
       setPersonaje(dato[0])
-      
+      // mostrar el componente frase
+      setMostrarSpinner(false)
     } catch (error) {
       console.log(error)
       //mostrar un msj al usuario de que ocurrio un error
     }
   }
+
+  //condicion logica if/ operador ternario
+  //(condicion logica)?(logica a ejecutar si cumplo la condicion logica):(codigo a ejecutar si no se cuple la condicion logica)
+  const mostrarComponente = (mostrarSpinner)?(<Spinner/>):(<Frase personaje={personaje}></Frase>)
 
   return (
     <>
@@ -40,7 +47,7 @@ function App() {
           />
         </div>
           <Button variant="warning" onClick={consultarAPI}>Obtener frase</Button>
-          <Frase personaje={personaje}></Frase>
+          {mostrarComponente}
       </Container>
     </>
   );
